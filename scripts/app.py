16efb5d895b3bf96df530b9fea3af1182ed75cb8
Ian Wang <ianwang.life@outlook.com>
2020-12-22 12:21:33 -0700

    Added comments to app.py and test.py scripts as practice

--- scripts/app.py
+++ scripts/app.py
@@ -4,3 +4,5 @@ def insult(victim, verb, noun):
     print("Hey {0}! You {1} like a {2}".format(victim, verb, noun))
 
 insult("there", "code", "monkey")
+
+# This is a comment, does not appear as output when running script
--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,2 @@
 print(8*8)
+# this is a comment, does not appear when script is run
